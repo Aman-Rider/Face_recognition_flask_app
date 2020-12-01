98a9587b3ed1d7aa9460b073719fcb1f592d72a1
unknown <aman94saxena@gmail.com>
2020-12-01 18:41:42 +0530

    Prediction attacted to flask app, Directory Restructuring

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from flask import render_template, redirect, request, url_for
 import os
-
+from PIL import Image
+import numpy as np
+from app.utils import pipeline_model
 UPLOAD_FOLDER = 'static/uploads'
+PREDICT_FOLDER = 'static/predict'
 def base():
     return render_template('base.html')
 
@@ -11,6 +14,12 @@ def index():
 def faceapp():
     return render_template("faceapp.html")
 
+def getwidth(path):
+    img = Image.open(path)
+    size = img.size # width and height
+    aspect = size[0]/size[1] # width / height
+    w = 300 * aspect
+    return int(w)
 
 def gender():
     if request.method =='POST':
@@ -18,4 +27,10 @@ def gender():
         filename = f.filename
         path = os.path.join(UPLOAD_FOLDER, filename)
         f.save(path)
-    return render_template("gender.html")
+        w = getwidth(path)
+        # prediction (pass to pipeline model)
+        pipeline_model(path,filename,color='bgr')
+
+
+        return render_template("gender.html", fileupload=True, img_name = filename, w=w)
+    return render_template("gender.html", fileupload = False, img_name = "freeai.png")
